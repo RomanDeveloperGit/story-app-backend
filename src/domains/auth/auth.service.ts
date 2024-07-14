a7212f9a7041dc5cace93065a886bcb5a81b613c
Roman <roman.developer@mail.ru>
2024-07-14 12:22:58 +0300

    feat(auth): add error codes

--- src/domains/auth/auth.service.ts
+++ src/domains/auth/auth.service.ts
@@ -5,6 +5,11 @@ import { User } from '@prisma/client';
 import { ConfigService } from '@/shared/config';
 import { UserService } from '@/domains/user/user.service';
 
+import {
+  ERROR_EMAIL_TAKEN,
+  ERROR_INVALID_REFRESH_TOKEN,
+  ERROR_USER_NOT_FOUND,
+} from './auth.error-codes';
 import { LogInRequest, LogInResponse } from './dto/log-in.dto';
 import { RefreshResponse } from './dto/refresh.dto';
 import { SignUpRequest, SignUpResponse } from './dto/sign-up.dto';
@@ -49,7 +54,10 @@ export class AuthService {
     const user = await this.userService.getUserByAuthData(data);
 
     if (!user) {
-      throw new BadRequestException("User doesn't exist");
+      throw new BadRequestException({
+        code: ERROR_USER_NOT_FOUND,
+        message: "User doesn't exist with this auth data",
+      });
     }
 
     const tokens = await this.getTokens(user);
@@ -58,6 +66,15 @@ export class AuthService {
   }
 
   async signUp(data: SignUpRequest): Promise<SignUpResponse> {
+    const isEmailTaken = Boolean(await this.userService.getUserByEmail(data.email));
+
+    if (isEmailTaken) {
+      throw new BadRequestException({
+        code: ERROR_EMAIL_TAKEN,
+        message: 'User already exists with this email',
+      });
+    }
+
     const user = await this.userService.create(data);
     const tokens = await this.getTokens(user);
 
@@ -68,7 +85,11 @@ export class AuthService {
     const user = await this.userService.getUserByEmail(data.email);
 
     if (!user) {
-      throw new BadRequestException("User doesn't exist");
+      // TODO: move this to refresh strategy
+      throw new BadRequestException({
+        code: ERROR_INVALID_REFRESH_TOKEN,
+        message: "User doesn't exist with this token data",
+      });
     }
 
     const tokens = await this.getTokens(user);
